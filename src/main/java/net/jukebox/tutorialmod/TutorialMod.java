ef0a995d386c5ace954e913b06c971a44f0f78dd
justjukebox <rhyssokollbusiness@gmail.com>
2024-09-10 19:35:29 -0500

    Ep #2

--- src/main/java/net/jukebox/tutorialmod/TutorialMod.java
+++ src/main/java/net/jukebox/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@
 package net.jukebox.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.jukebox.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -31,6 +33,8 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
 
 
         // Register the item to a creative tab
@@ -45,6 +49,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.ALEXANDRITE);
+        }
 
     }
 
