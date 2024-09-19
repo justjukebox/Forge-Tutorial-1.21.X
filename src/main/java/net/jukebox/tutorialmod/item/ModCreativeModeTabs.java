5bf10f42aabbc58666e296d942a7f834e8feb7b9
justjukebox <rhyssokollbusiness@gmail.com>
2024-09-18 20:09:16 -0500

    Ep #6 done!

--- src/main/java/net/jukebox/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/jukebox/tutorialmod/item/ModCreativeModeTabs.java
@@ -22,6 +22,7 @@ public class ModCreativeModeTabs {
                     .displayItems((itemDisplayParameters, output) ->
                     {
                         output.accept(ModItems.ALEXANDRITE.get());
+                        output.accept((ModItems.CHISEL.get()));
                     })
                     .build());
     public static final RegistryObject<CreativeModeTab> CRADLE_BLOCKS_TAB = CREATIVE_MODE_TABS.register("cradle_blocks_tab",
--- src/main/java/net/jukebox/tutorialmod/item/ModItems.java
+++ src/main/java/net/jukebox/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jukebox.tutorialmod.item;
 
 import net.jukebox.tutorialmod.TutorialMod;
+import net.jukebox.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,6 +12,8 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
     public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite", () -> new Item(new Item.Properties()));
+
+    public static final  RegistryObject<Item> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus)
     {
